43a6fa0d3513014af7dca695773fef756ecb35a0
rogerpuig <rogerpuigdemasa@gmail.com>
2022-06-11 12:42:12 +0200

    funcionality endPoint onePage

--- controller/onePage-controller.js
+++ controller/onePage-controller.js
@@ -1,5 +1,8 @@
-function onePage(req, res) {
-    res.send("onePage");
+const getData = require("../services/scraping");
+
+async function onePage(req, res) {
+    const data =await getData(1)
+    res.send(data);
 }
 
 module.exports = {
